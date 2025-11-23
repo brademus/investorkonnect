f631025e267be3162bb1a7690a585201f56544f8
braden heller <bradenheller@gmail.com>
2025-11-23 16:04:56 +0000

    File changes

--- src/pages/AdminApprovals.jsx
+++ src/pages/AdminApprovals.jsx
@@ -1,8 +1,8 @@
 import React, { useState, useEffect } from "react";
 import { useNavigate, Link } from "react-router-dom";
-import { createPageUrl } from "@/utils";
+import { createPageUrl } from "@/components/utils";
 import { base44 } from "@/api/base44Client";
-import { profileDedup, profileHealthCheck } from "@/api/functions";
+import { profileDedup, profileHealthCheck } from "@/components/functions";
 import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query";
 import { Button } from "@/components/ui/button";
 import { Badge } from "@/components/ui/badge";
